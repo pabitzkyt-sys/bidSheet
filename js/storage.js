ddcc118dd7bfd2b142deafe0d3ddc89c66ec3abb
pabitzkyt-sys <pabitzkyt@gmail.com>
2026-06-29 21:45:50 -0500

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,19 @@
 function getSettings() {
     return JSON.parse(localStorage.getItem("bidSettings")) || {};
 }
+
+function getCustomers() {
+    return JSON.parse(localStorage.getItem("customers")) || [];
+}
+
+function getParts() {
+    return JSON.parse(localStorage.getItem("partsLibrary")) || [];
+}
+
+function getEstimates() {
+    return JSON.parse(localStorage.getItem("estimates")) || [];
+}
+
+function saveEstimates(data) {
+    localStorage.setItem("estimates", JSON.stringify(data));
+}
